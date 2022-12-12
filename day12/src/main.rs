45dfd44e7fae813098d36af4f3a60b53d919e28e
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-12-12 21:40:27 +0100

    add solution to first puzzle from day12

---
+++ day12/src/main.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+fn can_climb(from: char, to: char) -> bool {
+    let from = if from == 'S' { 'a' } else { from };
+    let to = if to == 'E' { 'z' } else { to };
+    // dbg!(from);
+    // dbg!(to);
+    let from = from as u8;
+    let to = to as u8;
+    if to < from {
+        true
+    } else {
+        let elevation = to as u8 - from as u8;
+        elevation == 0 || elevation == 1
+    }
+}
+
+fn traverse(
+    map: &Vec<Vec<char>>,
+    current_position: (i32, i32),
+    current_step: i32,
+    end: (i32, i32),
+    visited: &mut HashMap<(i32, i32), i32>,
+) {
+    if let Some(v) = visited.get(&current_position) {
+        if v <= &current_step {
+            return;
+        }
+    }
+
+    visited.insert(current_position, current_step);
+
+    if current_position == end {
+        return;
+    }
+
+    // visited.insert(current_position, current_step);
+
+    let directions = [
+        (0, -1), // up
+        (1, 0),  // left
+        (0, 1),  // down
+        (-1, 0), //right
+    ];
+
+    for dir in directions {
+        let new_pos = (current_position.0 + dir.0, current_position.1 + dir.1);
+        if new_pos.0 < 0
+            || new_pos.0 >= map[0].len() as i32
+            || new_pos.1 < 0
+            || new_pos.1 >= map.len() as i32
+        {
+            continue;
+        }
+        if can_climb(
+            map[current_position.1 as usize][current_position.0 as usize],
+            map[new_pos.1 as usize][new_pos.0 as usize],
+        ) {
+            traverse(map, new_pos, current_step + 1, end, visited);
+        }
+    }
+}
+fn main() {
+    let input = std::fs::read_to_string("input").unwrap();
+    let map = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+    for (y, row) in map.iter().enumerate() {
+        if let Some(x) = row.iter().position(|c| *c == 'S') {
+            start = (x as i32, y as i32);
+        }
+
+        if let Some(x) = row.iter().position(|c| *c == 'E') {
+            end = (x as i32, y as i32);
+        }
+    }
+
+    let mut visited = HashMap::new();
+    traverse(&map, start, 0, end, &mut visited);
+
+    println!("{:?}", visited.get(&end));
+}
