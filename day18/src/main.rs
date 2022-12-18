3d45bf1994a309a1ffa564c926ec4a4b1687b931
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-12-18 22:56:58 +0100

    way simpler solution

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,46 +1,37 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 
 const SIDES: [(i32, i32, i32); 6] = [
-    (0, 0, 1),
+    (1, 0, 0),
     (-1, 0, 0),
     (0, 1, 0),
-    (0, 0, -1),
-    (1, 0, 0),
     (0, -1, 0),
+    (0, 0, 1),
+    (0, 0, -1),
 ];
 
 fn count_exposed(coords: &HashSet<(i32, i32, i32)>) -> usize {
-    let mut cubes = coords
+    coords
         .iter()
-        .map(|coords| (coords, [true; 6]))
-        .collect::<HashMap<_, _>>();
-
-    for cube in coords {
-        for (idx, side) in SIDES.iter().enumerate() {
-            let neighbour_coords = (cube.0 + side.0, cube.1 + side.1, cube.2 + side.2);
-            if let Some(cube) = cubes.get_mut(&neighbour_coords) {
-                cube[(idx + 3) % 6] = false;
-            } else {
-                continue;
-            }
-            cubes.get_mut(cube).unwrap()[idx] = false;
-        }
-    }
-
-    cubes
-        .values()
-        .map(|exposed| exposed.iter().filter(|e| **e).count())
+        .map(|c| {
+            SIDES
+                .iter()
+                .filter(|s| {
+                    let neighbour_coords = (c.0 + s.0, c.1 + s.1, c.2 + s.2);
+                    coords.get(&neighbour_coords).is_none()
+                })
+                .count()
+        })
         .sum()
 }
 
-fn remove_air_pockets(cubes: &HashSet<(i32, i32, i32)>) -> HashSet<(i32, i32, i32)> {
-    let (max_x, max_y, max_z) = cubes.iter().fold(
+fn remove_air_pockets(coords: &HashSet<(i32, i32, i32)>) -> HashSet<(i32, i32, i32)> {
+    let (max_x, max_y, max_z) = coords.iter().fold(
         (i32::MIN, i32::MIN, i32::MIN),
-        |(max_x, max_y, max_z), coords| {
+        |(max_x, max_y, max_z), (cx, cy, cz)| {
             (
-                std::cmp::max(max_x, coords.0),
-                std::cmp::max(max_y, coords.1),
-                std::cmp::max(max_z, coords.2),
+                std::cmp::max(max_x, *cx),
+                std::cmp::max(max_y, *cy),
+                std::cmp::max(max_z, *cz),
             )
         },
     );
@@ -53,7 +44,7 @@ fn remove_air_pockets(cubes: &HashSet<(i32, i32, i32)>) -> HashSet<(i32, i32, i3
         if c.0 < 0 || c.0 > max_x || c.1 < 0 || c.1 > max_y || c.2 < 0 || c.2 > max_z {
             continue;
         }
-        if cubes.contains(&c) {
+        if coords.contains(&c) {
             continue;
         } else if water.insert(c) {
             SIDES.iter().for_each(|s| {
