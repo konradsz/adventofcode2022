3da9caa7887acad78d1d332ae1af1ecbfecb38e3
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-12-28 17:00:32 +0100

    refactor

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -14,26 +14,17 @@ struct Range {
 impl Report {
     fn get_coverage_at_row(self, y: i64) -> Option<Range> {
         let vertical_distance = (self.sensor_y - y).abs();
-        // use then_some
-        if vertical_distance > self.radius {
-            None
-        } else {
-            Some(Range {
-                start: self.sensor_x - (self.radius - vertical_distance),
-                end: self.sensor_x + (self.radius - vertical_distance),
-            })
-        }
+        (vertical_distance <= self.radius).then_some(Range {
+            start: self.sensor_x - (self.radius - vertical_distance),
+            end: self.sensor_x + (self.radius - vertical_distance),
+        })
     }
 }
 
 fn merge_ranges(ranges: &mut Vec<Range>) {
     ranges.sort();
 
-    'outer: loop {
-        if ranges.len() == 1 {
-            break;
-        }
-
+    'outer: while ranges.len() > 1 {
         for i in 0..ranges.len() - 1 {
             if let Some(merged) = merge(ranges[i], ranges[i + 1]) {
                 let (left, right) = ranges.split_at(i);
@@ -52,26 +43,17 @@ fn merge_ranges(ranges: &mut Vec<Range>) {
 }
 
 fn merge(lhs: Range, rhs: Range) -> Option<Range> {
-    if rhs.start - 1 <= lhs.end {
-        Some(Range {
-            start: std::cmp::min(lhs.start, rhs.start),
-            end: std::cmp::max(lhs.end, rhs.end),
-        })
-    } else {
-        None
-    }
+    (rhs.start - 1 <= lhs.end).then_some(Range {
+        start: std::cmp::min(lhs.start, rhs.start),
+        end: std::cmp::max(lhs.end, rhs.end),
+    })
 }
 
 fn part_1(reports: &[Report]) -> i64 {
-    const ROW: i64 = 2000000;
-    let mut ranges = vec![];
-    for report in reports {
-        let cov = report.get_coverage_at_row(ROW);
-
-        if let Some(range) = cov {
-            ranges.push(range);
-        }
-    }
+    let mut ranges = reports
+        .into_iter()
+        .filter_map(|report| report.get_coverage_at_row(2000000))
+        .collect();
 
     merge_ranges(&mut ranges);
     ranges[0].start.abs() + ranges[0].end.abs()
@@ -79,14 +61,10 @@ fn part_1(reports: &[Report]) -> i64 {
 
 fn part_2(reports: &[Report]) -> i64 {
     for row in 0..=4000000 {
-        let mut ranges = vec![];
-        for report in reports {
-            let cov = report.get_coverage_at_row(row);
-
-            if let Some(range) = cov {
-                ranges.push(range);
-            }
-        }
+        let mut ranges = reports
+            .into_iter()
+            .filter_map(|report| report.get_coverage_at_row(row))
+            .collect();
 
         merge_ranges(&mut ranges);
         if ranges.len() != 1 {
@@ -98,25 +76,26 @@ fn part_2(reports: &[Report]) -> i64 {
 
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
-    let mut reports = vec![];
-    for line in input.lines() {
-        let mut s = line.split(&['=', ',', ':']);
-        s.next();
-        let sensor_x = s.next().unwrap().parse::<i64>().unwrap();
-        s.next();
-        let sensor_y = s.next().unwrap().parse::<i64>().unwrap();
-        s.next();
-        let beacon_x = s.next().unwrap().parse::<i64>().unwrap();
-        s.next();
-        let beacon_y = s.next().unwrap().parse::<i64>().unwrap();
+    let reports = input
+        .lines()
+        .map(|line| {
+            let mut s = line.split(&['=', ',', ':']);
+            s.next();
+            let sensor_x = s.next().unwrap().parse::<i64>().unwrap();
+            s.next();
+            let sensor_y = s.next().unwrap().parse::<i64>().unwrap();
+            s.next();
+            let beacon_x = s.next().unwrap().parse::<i64>().unwrap();
+            s.next();
+            let beacon_y = s.next().unwrap().parse::<i64>().unwrap();
 
-        let r = Report {
-            sensor_x,
-            sensor_y,
-            radius: (sensor_x - beacon_x).abs() + (sensor_y - beacon_y).abs(),
-        };
-        reports.push(r);
-    }
+            Report {
+                sensor_x,
+                sensor_y,
+                radius: (sensor_x - beacon_x).abs() + (sensor_y - beacon_y).abs(),
+            }
+        })
+        .collect::<Vec<_>>();
 
     assert_eq!(part_1(&reports), 5564017);
     assert_eq!(part_2(&reports), 11558423398893);
